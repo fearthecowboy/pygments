42532678863ba1feb2924bbac63118776a30dd83
Garrett Serack <gserack@gmail.com>
2013-09-24 16:47:23 -0700

    updated license

--- test/Program.cs
+++ test/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace test {
     class Program {
@@ -13,6 +7,7 @@ namespace test {
             var highlighter = new Pygments.Highlighter();
             var src = File.ReadAllText(@"..\..\..\..\..\test\program.cs");
             File.WriteAllText("output.html", highlighter.HighlightToHtml(src, "c#", "vs", highlightLines: "7 8 9 10"));
+
         }
     }
 }
